d5eb7c41a700accd89c5293deae571efc4f8a480
Sunil Sharma <sunilss395@gmail.com>
2013-10-10 12:49:38 +0530

    initial commit

---
+++ etc/rbenv.d/which/rbx2.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+if [ ! -x "$RBENV_COMMAND_PATH" ] && [[ $RBENV_VERSION =~ rbx ]]; then
+export RBENV_COMMAND_PATH="${RBENV_ROOT}/versions/${RBENV_VERSION}/gems/bin/${RBENV_COMMAND}"
+fi
